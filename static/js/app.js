d93aaf9259c5c46661bd9c477395cf1ce5fb57e9
Filipe-uefs <lipe_0109@hotmail.com>
2021-08-04 00:34:02 -0300

    update layout and show new link

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,28 @@
 
 const bnt = document.getElementById("send")
-bnt.addEventListener("click", function(e){
-	e.preventDefault();
+bnt.addEventListener("click", function (e) {
+    e.preventDefault();
     var link = document.getElementById("link").value;
     link = link.trim()
-    if(link==""){
-    	alert("Link requerido")
-    	return
+    if (link == "") {
+        alert("Link requerido")
+        return
     }
-    async function registerLink(){
-    try {
-        const response = await fetch("http://127.0.0.1:5000/registerLink", {
-        method: 'POST',
-        headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({link: link})
-        })
-        const data = await response.json()
-        console.log(data)
-       } catch(error){
-        console.error("erro")
-       }
+    async function registerLink() {
+        try {
+            const response = await fetch("http://127.0.0.1:5000/registerLink", {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ link: link })
+            })
+            const data = await response.json()
+            document.getElementById("display-link").innerHTML = `Link http://127.0.0.1:5000/${data["index"]}`; 
+        } catch (error) {
+            console.error("erro")
+        }
     }
     registerLink()
 })
